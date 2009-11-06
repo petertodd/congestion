e3740ec735c98dccdc2165fcdaa71ce8c6c635ca
Peter Todd <peter.todd@gedex.ca>
2009-11-06 09:24:48 -0500

    Now seeds the RNG with time()

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 #include <world.h>
 #include <display.h>
 
 int main(int argc,char *argv[]){
+    srandom(time(NULL));
+
     init_world();
     init_display();
 
